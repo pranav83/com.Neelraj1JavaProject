dd0a5b2c560739a8441f60c1e3765435a09b4338
pranav83 <pranavqa1@gmail.com>
2020-05-22 20:33:27 +0100

    Initial commit

---
+++ src/Demo1/Hello.java
@@ -0,0 +1,16 @@
+package Demo1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String name = "Harry";
+        System.out.println(name);
+        int a = 45, x = 56, y = 67;
+        float b = 45.22f;
+        boolean isAdult = false;
+        System.out.println(x);
+        System.out.println(b);
+        System.out.println(isAdult);
+
+    }
+}
